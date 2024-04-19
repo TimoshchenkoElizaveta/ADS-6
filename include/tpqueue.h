94ae3d6649c2bd36e48a3395d6846594f82edd38
DisneyLib <160058897+TimoshchenkoElizaveta@users.noreply.github.com>
2024-04-19 19:32:46 +0300

    Update tpqueue.h

--- include/tpqueue.h
+++ include/tpqueue.h
@@ -3,13 +3,44 @@
 #define INCLUDE_TPQUEUE_H_
 
 template<typename T, int size>
-class TPQueue {
-  // реализация шаблона очереди с приоритетом на кольцевом буфере
-};
-
 struct SYM {
   char ch;
   int prior;
 };
+class TPQueue {
+ private:
+    int c;
+    int begin, end;
+    SYM* arr;
+
+ public:
+    TPQueue() : cnt(0), begin(0), end(0), data(new T[size]) {}
+
+    T pop() {
+        if (cnt == 0) {
+            throw std::string("Empty");
+        } else {
+            count--;
+            return data[begin++ % size];
+        }
+    }
+    void push(const T &item) {
+        if (cnt >= size)
+            throw std::string("Full");
+      cnt++;
+      int Index = end;
+      for (int i = begin; i < end; i++) {
+          if (data[i].prior < item.prior) {
+              Index = i;
+              break;
+          }
+      }
+      for (int i = end; i > Index; i--)
+          data[i % size] = data[(i - 1) % size];
+              data[Index % size] = item;
+        end++;
+    }
+};
+
 
 #endif  // INCLUDE_TPQUEUE_H_
